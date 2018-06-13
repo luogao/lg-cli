9164be4467cdf12c1da46a031886194378a1f36a
罗高 <luogao_lg@sina.com>
2018-06-13 23:49:05 +0800

    fix problems

--- bin/lg.js
+++ bin/lg.js
@@ -1,12 +1,27 @@
+#!/usr/bin/env node
 const program = require('commander')
 program
-	.version(require('../package').version)
-	.usage('<command> [options]')
+  .version(require('../package').version)
+  .usage('<command> [options]')
 
 program
-	.command('init <app-name>')
-	.description('create a new project powered by lg-cli')
-	.action((name, cmd) => {
-		console.log(name. cmd)
-		// require('../lib/init')(name)
-	})
+  .command('init <app-name>')
+  .description('create a new project powered by lg-cli')
+  .action((name, cmd) => {
+    console.log(name.cmd)
+    // require('../lib/init')(name)
+  })
+
+program
+  .command('test <app-name>')
+  .description('this is a test')
+  .option('-p, --preset <presetName>', 'Skip prompts and use saved or remote preset')
+  .action((name, cmd) => {
+    require('../lib/test')(name, cmd)
+  })
+
+program.parse(process.argv)
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp()
+}
