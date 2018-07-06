b34e5da13159b0f64c8075b32eb836db3f0df3bb
Roy <luogao_lg@sina.com>
2018-07-06 18:29:55 +0800

    更新 init 指令

--- bin/lg.js
+++ bin/lg.js
@@ -5,11 +5,10 @@ program
   .usage('<command> [options]')
 
 program
-  .command('init <app-name>')
+  .command('init <template> <app-name>')
   .description('create a new project powered by lg-cli')
-  .action((name, cmd) => {
-    console.log(name.cmd)
-    // require('../lib/init')(name)
+  .action(() => {
+    require('../lib/init')()
   })
 
 program
