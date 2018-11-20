b0dffb13ac9af44e88039e31f0d383d86fb25d45
luogao <luogao_lg@sina.com>
2018-11-20 16:23:48 +0800

    添加react init 处理，优化init 成功提示

---
+++ test-react/src/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+ReactDOM.render(
+  <h1 className="welcome">Welcome To LG</h1>,
+  document.getElementById('root')
+)
